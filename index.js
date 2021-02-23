5847a25fff3b69e36f7c2ddbf08c5049ddcaf937
Jason Herrera <baytownjason@yahoo.com>
2021-02-23 16:24:14 -0600

    commit tree 45413fceaef63c7668053d3dbf47f827c9788404

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function changeDiv(text){
+    const locationoo = document.querySelector("body")
+    locationoo.innerText = text
+}
+  
+function writeCards(arrayOfNames, event){
+    let array10 = []
+    for (let i = 0; i < arrayOfNames.length; i++){
+        array10.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`)
+    } return array10
+}
+function countDown(positiveInt){
+    while (positiveInt >= 0){
+        console.log(positiveInt)
+        positiveInt -=1
+    }
+}
+// let arr1 = ['Jason', "Astrid", 'Xhiloh', "Bane"]
+
+// changeDiv(writeCards(arr1, "birthday"));
+// debugger
+
